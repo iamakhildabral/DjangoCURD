0185286dc85f5bdbf0a5d96b912882f5825fb10e
iamakhildabral <akhildabral02@gmail.com>
2023-06-12 20:46:32 +0530

    added search and filter functionality

--- api/admin.py
+++ api/admin.py
@@ -1,15 +1,18 @@
 from django.contrib import admin
-from .models import Company,Employee
+from .models import Company, Employee
 
 class CompanyAdmin(admin.ModelAdmin):
-    list_display = ('name','location','type','active')
+    list_display = ('name', 'location', 'type', 'active')
     search_fields = ('name',)
+    list_filter = ('type', 'location', 'active')
+
 
 class EmployeeAdmin(admin.ModelAdmin):
-    list_display = ('name','email','company')
-    search_fields = ('name','email',)
+    list_display = ('name', 'email', 'company')
+    search_fields = ('name', 'email',)
+    list_filter = ('company', 'email',)
+
 
 # Register your models here.
-admin.site.register(Company,CompanyAdmin)
-admin.site.register(Employee,EmployeeAdmin)
-
+admin.site.register(Company, CompanyAdmin)
+admin.site.register(Employee, EmployeeAdmin)
