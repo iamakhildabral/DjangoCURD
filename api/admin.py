632a4bce6a76ba4e529b40a6352ac98bec509be5
iamakhildabral <akhildabral02@gmail.com>
2023-06-12 20:41:47 +0530

    added the admin/search customization for the Company as well as Employee

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Company,Employee
+
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ('name','location','type','active')
+    search_fields = ('name',)
+
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ('name','email','company')
+    search_fields = ('name','email',)
 
 # Register your models here.
+admin.site.register(Company,CompanyAdmin)
+admin.site.register(Employee,EmployeeAdmin)
+
